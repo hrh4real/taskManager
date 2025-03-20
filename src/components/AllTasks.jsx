46fbb5fdd5c065d916545227bd3e3904bd3b5ce7
hrh4real <himanshuhada167@gmail.com>
2025-03-20 11:14:22 +0530

    Added mark as done button

--- src/components/AllTasks.jsx
+++ src/components/AllTasks.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Tasks({ tasks }) {
+    const [completedTasks, setCompletedTasks] = useState([]);
+    const [clickedTaskIndex, setClickedTaskIndex] = useState(null);
+    function handleTaskClick(index) {
+        setClickedTaskIndex(clickedTaskIndex === index ? null : index)
+    }
+    function handleMarkButton(index) {
+        setCompletedTasks([...completedTasks, index]);
+        setClickedTaskIndex(null)
+    }
     const formatDate = (dateString) => {
         const options = { day: 'numeric', month: 'short', year: 'numeric' }
         return new Date(dateString).toLocaleDateString('en-US', options);
@@ -15,13 +25,40 @@ export default function Tasks({ tasks }) {
         <section className="tasks-section">
             <ul className="tasks-list">
                 {tasks.map((task, index) => (
-                    <li className="task-item" key={index}>
-                        <span className="task-name">{task.name}</span>
-                        <div>
-                            <span className="task-date">{formatDate(task.date)}</span>
-                            <span className="task-time">{formatTime(task.time)}</span>
+                    !completedTasks.includes(index) && (
+                        <li onClick={() => handleTaskClick(index)} className="task-item" key={index}>
+                        <div className="task-item-info">
+                            <span className="task-name">{task.name}</span>
+                            <div>
+                                <span className="task-date">{formatDate(task.date)}</span>
+                                <span className="task-time">{formatTime(task.time)}</span>
+                            </div>
                         </div>
+                        {clickedTaskIndex === index && (
+                            <div>
+                                <button
+                                    onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleMarkButton(index);
+                                    }}
+                                    
+                                >
+                                    Mark As Done
+                                </button>
+                                <button
+                                    onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleMarkButton(index);
+                                    }}
+                                    
+                                >
+                                    Mark As Done
+                                </button>
+                            </div>
+                            )
+                        }
                     </li>
+                    )
                 ))}
             </ul>
         </section>
